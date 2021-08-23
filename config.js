c70019234735e8e01650d1efbb6b97fe72c907fe
reisanhd2021 <88941369+reisanhd2021@users.noreply.github.com>
2021-08-23 13:03:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
-global.owner = ['6281390658325', '6282256115584', '6285713964963']// Put your number here
-global.mods = ['13479805233'] // Want some help?
+global.owner = ['6281806695954', '6282256115584', '6285713964963']// Put your number here
+global.mods = ['6281806695954'] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -24,8 +24,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot Is The Best'
+global.packname = 'Reisan Gans'
+global.author = 'ReiBot Is The Best'
 
 global.multiplier = 39 // The higher, The harder levelup
 
